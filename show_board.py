4eb2c10323434e39a5b1b8dc8f2f189a14479bf4
RodrigoPinto22 <131172504+RodrigoPinto22@users.noreply.github.com>
2025-03-24 14:17:37 +0000

    Delete show_board.py

--- show_board.py
+++
@@ -1,9 +0,0 @@
-def print_jogo(jogo, board_size):
-    mid_index = int(board_size/2)
-    linha_superior = jogo[:mid_index]  # Pegamos os 3 primeiros ramos (lado esquerdo)
-    linha_inferior = jogo[mid_index:]  # Pegamos os 3 últimos ramos (lado direito)
-
-    print("-----------------------------------------")
-
-    for i in range(mid_index):  # Percorre cada "coluna" do jogo
-        print(f"{linha_superior[i][::-1]}    |    {linha_inferior[i]}")  # Mantemos as colunas corretas
