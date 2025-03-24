5b7dc32f7a48403789f248c237946a6d69028afa
RodrigoPinto22 <131172504+RodrigoPinto22@users.noreply.github.com>
2025-03-24 15:17:42 +0000

    Update game.py

--- game.py
+++ game.py
@@ -14,36 +14,31 @@ def print_jogo(jogo, board_size):
 def make_move(board_info):
     board = board_info[0]
     board_size = board_info[1]
-    #INDEX de 1 - 7
+    #INDEX de 1 - 8
     while True:
         print_jogo(board, board_size)
 
         try:
             idx1 = int(input("Select the first branch: "))
             idx2 = int(input("Select the second branch: "))
-            if idx1 >= board_size or idx2 >= board_size:
+            if idx1 > board_size or idx2 > board_size:
                 print("Unexpected value, try again")
                 continue
-            check_move(idx1, idx2, board)
+            check_move(idx1-1, idx2-1, board)
         except ValueError:
             print("Unexpected value, try again")
 
 
 def check_move(b1, b2, board):
-    #while True:
     print(board[0])
     consecutive_values = get_consecutive(board[b1])
     if board[b2][0] != 0:
         print("Impossible Move! Try another one")
         return
     elif board[b2][3] == 0:
-        i = 0
-        while board[b1][i] == 0:
-            continue
-        c = 0
+        print(f"consec = {consecutive_values}")
         board[b2][3] = board[b1][0]
-
-
+        board[b1][0] = 0
 
     return b1, b2
 
